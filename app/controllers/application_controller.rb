a82007f887bd187733c840c0185ddeff87376385
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-11-14 13:51:44 -0700

    Completed the lab.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
-class ApplicationController < Sinatra::Base
 
+
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
   # add routes
+  
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+   bakery = Bakery.all.find(params[:id])
+   bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    goods = BakedGood.all.order(price: :desc)
+    goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    pricy = BakedGood.all.max_by {|item| item.price}
+    pricy.to_json
+  end
+
+
 
 end
